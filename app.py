fad88470db2a7e61d3e79add08a9c3b961efe7aa
Rajesh-snippet <rsarmabordoloi@gmail.com>
2026-04-05 22:27:02 +0530

    PDf query

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
+
